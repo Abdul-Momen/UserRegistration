461e965928d89dff8b394242698982cf728e525d
Abdul Momen <abdulmomen0099@gmail.com>
2019-11-27 18:45:47 +0600

    First commit in project

---
+++ app/src/main/java/com/momen/momen/HomeFragment.java
@@ -0,0 +1,63 @@
+package com.momen.momen;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    private TextView emailTV,phoneTV,nameTV,passTV;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        String email = getArguments().getString("email");
+        String phone = getArguments().getString("phone");
+        String name = getArguments().getString("name");
+        String password = getArguments().getString("pass");
+
+        emailTV = view.findViewById(R.id.emailTV);
+        emailTV.setText(email);
+
+        phoneTV = view.findViewById(R.id.phoneTV);
+        phoneTV.setText(phone);
+
+
+        nameTV = view.findViewById(R.id.nameTV);
+        nameTV.setText(name);
+
+        passTV = view.findViewById(R.id.passwordTV);
+        passTV.setText(password);
+
+
+        return;
+
+    }
+}
